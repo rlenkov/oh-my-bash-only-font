8a61ead5214cce7c0f083c52f0927691481b306d
Richard Lenkovits <richard.lenkovits@gmail.com>
2023-05-14 14:43:39 +0200

    Create gutted version of omb

---
+++ setup-prompt.inc.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+echo "$(dirname -- $0)"
+echo "${BASH_SOURCE[0]}"
+
+base_dir="$(dirname "${BASH_SOURCE[0]}")"
+
+source "${base_dir}/utils.sh"
+source "${base_dir}/omb-deprecate.sh"
+source "${base_dir}/omb-prompt-colors.sh"
+source "${base_dir}/omb-prompt-base.sh"
+
+SCM_NONE_CHAR=''
+SCM_THEME_PROMPT_DIRTY=" ${_omb_prompt_brown}✗"
+SCM_THEME_PROMPT_CLEAN=""
+SCM_THEME_PROMPT_PREFIX="${_omb_prompt_green}|"
+SCM_THEME_PROMPT_SUFFIX="${_omb_prompt_green}|"
+SCM_GIT_SHOW_MINIMAL_INFO=true
+
+CLOCK_THEME_PROMPT_PREFIX=''
+CLOCK_THEME_PROMPT_SUFFIX=' '
+THEME_SHOW_CLOCK=${THEME_SHOW_CLOCK:-"true"}
+THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$_omb_prompt_bold_navy"}
+THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%I:%M:%S"}
+
+OMB_PROMPT_VIRTUALENV_FORMAT='(%s) '
+OMB_PROMPT_SHOW_PYTHON_VENV=${OMB_PROMPT_SHOW_PYTHON_VENV:=true}
+
+function _omb_theme_PROMPT_COMMAND() {
+    # This needs to be first to save last command return code
+    local RC="$?"
+
+    local hostname="${_omb_prompt_bold_gray}\u@\h"
+    local python_venv; _omb_prompt_get_python_venv
+    python_venv=$_omb_prompt_white$python_venv
+
+    # Set return status color
+    if [[ ${RC} == 0 ]]; then
+        ret_status="${_omb_prompt_bold_green}"
+    else
+        ret_status="${_omb_prompt_bold_brown}"
+    fi
+
+    # Append new history lines to history file
+    history -a
+
+    PS1="$(clock_prompt)$python_venv${hostname} ${_omb_prompt_bold_teal}\W $(scm_prompt_char_info)${ret_status}→ ${_omb_prompt_normal}"
+}
+
+_omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
